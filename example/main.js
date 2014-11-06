1c62ea5b06c8437bfd94c7a2d12fd000e6b4b362
Jack Callister <jarsbe@gmail.com>
2014-11-06 21:11:52 +1300

    Add an alert for the values.

--- example/main.js
+++ example/main.js
@@ -7,13 +7,21 @@ var React = require('react'),
 
 var App = React.createClass({
 
+  submit: function() {
+    var polyselect = this.refs.polyselect
+    var input = polyselect.refs.value
+  },
+
   render: function() {
     return (
-      <Polyselect search={true}>
-        <Polyoption title="My select option" value="1" />
-        <Polyoption title="My second select option" value="2" />
-        <Polyoption title="My third select option" value="3" />
-      </Polyselect>
+      <div>
+        <Polyselect ref="polyselect" search={true}>
+          <Polyoption title="My select option" value="1" />
+          <Polyoption title="My second select option" value="2" />
+          <Polyoption title="My third select option" value="3" />
+        </Polyselect>
+        <button onClick={this.submit}>Submit</button>
+      </div>
     );
   }
 });
